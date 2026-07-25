963800cd785068d35174521d33f8c7a7de8258d9
ayaans16 <ashaik26@uoguelph.ca>
2026-07-25 19:31:23 -0400

    Remove old python/ and electron/ directories
    These were renamed to core/ and ui/ but the deletions were never committed.

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  saveFile: (suggestedName, data) =>
-    ipcRenderer.invoke("save-file", suggestedName, data),
-});
